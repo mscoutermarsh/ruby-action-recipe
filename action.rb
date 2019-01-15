819c93cf69e394204062a83da26cc697f23dda86
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2019-01-14 17:07:18 -0800

    Update action.rb

--- action.rb
+++ action.rb
@@ -8,9 +8,10 @@ puts event.inspect
 # Use GITHUB_TOKEN to interact with the GitHub API
 client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
 
-current_repo = Octokit::Repository.from_url(event["issue"]["pull_request"]["html_url"])
+current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
 
 # Let's grab all the issues for the current repo
 issues = client.issues(current_repo)
 
+puts "***Printing out this repo's issues***"
 puts issues.inspect
